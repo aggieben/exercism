bf0556205a7ce3477e0c17fd5e047272d977523c
Benjamin Collins <aggieben@gmail.com>
2023-01-06 20:46:13 -0600

    reduce to one macro

--- rust/space-age/src/lib.rs
+++ rust/space-age/src/lib.rs
@@ -21,93 +21,39 @@ pub trait Planet {
     }
 }
 
-const SECONDS_IN_YEAR : f64 = 31536000.0;
+const SECONDS_IN_YEAR : u64 = 31557600;
+
+macro_rules! planet_impl {
+    ($($planet_type:ty, $orbital_period:literal),+) => {
+        $(
+            impl $planet_type {
+                const ORBITAL_PERIOD : f64 = $orbital_period;
+            }
+            impl Planet for $planet_type {
+                fn years_during(d: &Duration) -> f64 {
+                    d.seconds as f64 / (SECONDS_IN_YEAR as f64 * Self::ORBITAL_PERIOD)
+                }
+            }
+        )*
+    };
+}
 
 pub struct Mercury;
-impl Mercury {
-    const ORBITAL_PERIOD : f64 = 0.2408467;
-}
-
 pub struct Venus;
-impl Venus {
-    const ORBITAL_PERIOD : f64 = 0.061519726;
-}
-
 pub struct Earth;
-impl Earth {
-    const ORBITAL_PERIOD : f64 = 1.0;
-}
-
 pub struct Mars;
-impl Mars {
-    const ORBITAL_PERIOD : f64 = 1.8808158;
-}
-
 pub struct Jupiter;
-impl Jupiter {
-    const ORBITAL_PERIOD : f64 = 11.862615;
-}
-
 pub struct Saturn;
-impl Saturn {
-    const ORBITAL_PERIOD : f64 = 29.447498;
-}
-
 pub struct Uranus;
-impl Uranus {
-    const ORBITAL_PERIOD : f64 = 84.016846;
-}
-
 pub struct Neptune;
-impl Neptune {
-    const ORBITAL_PERIOD : f64 = 164.79132;
-}
 
-
-impl Planet for Mercury {
-    fn years_during(d: &Duration) -> f64 {
-        d.seconds as f64 / SECONDS_IN_YEAR * Self::ORBITAL_PERIOD
-    }
-}
-
-impl Planet for Venus {
-    fn years_during(d: &Duration) -> f64 {
-        d.seconds as f64 / SECONDS_IN_YEAR * Self::ORBITAL_PERIOD
-    }
-}
-
-impl Planet for Earth {
-    fn years_during(d: &Duration) -> f64 {
-        d.seconds as f64 / SECONDS_IN_YEAR * Self::ORBITAL_PERIOD
-    }
-}
-
-impl Planet for Mars {
-    fn years_during(d: &Duration) -> f64 {
-        d.seconds as f64 / SECONDS_IN_YEAR * Self::ORBITAL_PERIOD
-    }
-}
-
-impl Planet for Jupiter {
-    fn years_during(d: &Duration) -> f64 {
-        d.seconds as f64 / SECONDS_IN_YEAR * Self::ORBITAL_PERIOD
-    }
-}
-
-impl Planet for Saturn {
-    fn years_during(d: &Duration) -> f64 {
-        d.seconds as f64 / SECONDS_IN_YEAR * Self::ORBITAL_PERIOD
-    }
-}
-
-impl Planet for Uranus {
-    fn years_during(d: &Duration) -> f64 {
-        d.seconds as f64 / SECONDS_IN_YEAR * Self::ORBITAL_PERIOD
-    }
-}
-
-impl Planet for Neptune {
-    fn years_during(d: &Duration) -> f64 {
-        d.seconds as f64 / SECONDS_IN_YEAR * Self::ORBITAL_PERIOD
-    }
-}
+planet_impl!(
+    Mercury, 0.2408467,
+    Venus, 0.61519726,
+    Earth, 1.00000000000,
+    Mars, 1.8808158,
+    Jupiter, 11.862615,
+    Saturn, 29.447498,
+    Uranus, 84.016846,
+    Neptune, 164.79132
+);
